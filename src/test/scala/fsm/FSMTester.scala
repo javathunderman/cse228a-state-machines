6648246c5d4a64c3711643604bfdd5a0127d0767
Arjun <javathunderman@outlook.com>
2025-05-26 17:33:16 -0700

    add another FSM diagram

--- src/test/scala/fsm/FSMTester.scala
+++ src/test/scala/fsm/FSMTester.scala
@@ -50,4 +50,48 @@ class FSMTester extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.out.expect(2)
     }
   }
+  it should "handle multiple possible transitions" in {
+    val fsm_graph = FSMGraph("src/test/scala/fsm/sample_2.dot")
+    test(new FSM(fsm_graph)) { dut =>
+      dut.io.in(0).poke(true.B)
+      dut.io.in(1).poke(false.B)
+      dut.io.in(2).poke(false.B)
+      dut.io.in(3).poke(false.B)
+      dut.io.in(4).poke(false.B)
+      dut.clock.step()
+      dut.io.out.expect(1)
+
+      dut.io.in(0).poke(false.B)
+      dut.io.in(1).poke(false.B)
+      dut.io.in(2).poke(false.B)
+      dut.io.in(3).poke(true.B)
+      dut.io.in(4).poke(false.B)
+      dut.clock.step()
+      dut.io.out.expect(3)
+
+      dut.io.in(0).poke(false.B)
+      dut.io.in(1).poke(false.B)
+      dut.io.in(2).poke(false.B)
+      dut.io.in(3).poke(false.B)
+      dut.io.in(4).poke(true.B)
+      dut.clock.step()
+      dut.io.out.expect(1)
+
+      dut.io.in(0).poke(false.B)
+      dut.io.in(1).poke(true.B)
+      dut.io.in(2).poke(false.B)
+      dut.io.in(3).poke(false.B)
+      dut.io.in(4).poke(false.B)
+      dut.clock.step()
+      dut.io.out.expect(2)
+      
+      dut.io.in(0).poke(false.B)
+      dut.io.in(1).poke(false.B)
+      dut.io.in(2).poke(true.B)
+      dut.io.in(3).poke(false.B)
+      dut.io.in(4).poke(false.B)
+      dut.clock.step()
+      dut.io.out.expect(2)
+    }
+  }
 }
